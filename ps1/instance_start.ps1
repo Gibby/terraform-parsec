bd814f68fe0b50096395fca708edb175c3091f5d
Chris Gibson <git@twoitguys.com>
2019-01-29 12:08:08 -0500

    Updates

--- ps1/instance_start.ps1
+++ ps1/instance_start.ps1
@@ -1,5 +1,6 @@
 # Custom vars
 $SourceRepo = "https://raw.githubusercontent.com/Gibby/terraform-parsec/master/ps1"
+$InitScript = "instance_start.ps1"
 $ScriptsToRun = @("initialise-disks.ps1", "bind-steam-library.ps1", "extra-apps.ps1")
 
 # Get Current date/time
@@ -17,10 +18,12 @@ if (-not (Test-Path -Path $ScriptDir)) {
 $LogFile = "${ScriptDir}\logs.${DateTime}.txt"
 Add-Content -Value "Starting custom scripts..." -Path $LogFile
 
+# Pull the init script down if needed again
+Invoke-WebRequest -Uri "${SourceRepo}/${InitScript}" -OutFile "${ScriptDir}/${InitScript}" *> $LogFile
 
 # Download and run scripts
 Foreach ($script in $ScriptsToRun)
 {
-  Invoke-WebRequest -Uri "${SourceRepo}/${script}" -OutFile "${ScriptDir}/${script}"
-  & "${ScriptDir}/${script}"
+  Invoke-WebRequest -Uri "${SourceRepo}/${script}" -OutFile "${ScriptDir}/${script}" *> $LogFile
+  & "${ScriptDir}/${script}" *> $LogFile
 }
