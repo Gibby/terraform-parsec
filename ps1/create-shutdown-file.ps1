a08e094a7118db230c3141fc344baa5a75e2cc63
Chris Gibson <git@twoitguys.com>
2019-02-01 16:26:47 -0500

    Updates

--- ps1/create-shutdown-file.ps1
+++ ps1/create-shutdown-file.ps1
@@ -11,5 +11,8 @@ $OutFile = "C:\Users\Administrator\Desktop\shutdown.bat"
 
 $FileContent = @"
 Powershell.exe -executionpolicy remotesigned -File C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\InitializeInstance.ps1 -Schedule
+
+
 shutdown.exe /s /t 00
+
 "@ | Out-File $OutFile
