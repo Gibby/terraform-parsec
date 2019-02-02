bf9e9fb3fdc57a42998dc1e78e6e85e374940dcf
Chris Gibson <git@twoitguys.com>
2019-02-02 15:06:40 -0500

    Updates

--- ps1/create-shutdown-file.ps1
+++ ps1/create-shutdown-file.ps1
@@ -3,9 +3,12 @@ $OutFile = "C:\Users\Administrator\Desktop\shutdown.bat"
 $FileContent = @"
 Powershell.exe -executionpolicy remotesigned -File C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\InitializeInstance.ps1 -Schedule
 
+
 @RD /S /Q "C:\terraform-parsec"
 
+
 shutdown.exe /s /t 00
+
 
 "@
 Set-Content -Encoding ASCII -Value $FileContent -Path $OutFile
