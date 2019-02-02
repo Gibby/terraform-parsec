bc6606f30d61338f54d91ace277732a8b228f64c
Chris Gibson <git@twoitguys.com>
2019-02-02 14:03:14 -0500

    Updates

--- ps1/set-hostname.ps1
+++ ps1/set-hostname.ps1
@@ -1,3 +1,4 @@
+C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\InitializeInstance.ps1 -Schedule
 $instanceid = (curl http://169.254.169.254/latest/meta-data/instance-id | Select Content ).Content
 $ec2_tag_hostname = (aws --region us-east-1 ec2 describe-instances --instance-ids "$instanceid" --query 'Reservations[*].Instances[*].[Tags[?Key==`Hostname`].Value]' --output=text)
 if ($env:computername -ne $ec2_tag_hostname) {
